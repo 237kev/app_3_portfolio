22f978f842e7caa939ec75baf64aeabd3bdbce17
Kevin <k3v1n.k4md3m@gmail.com>
2025-03-06 12:50:33 +0100

    image description and links added

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import streamlit as st
+import pandas
 
-st.set_page_config(layout='wide')
-col1, col2 = st.columns([1, 6])  # Adjust column width ratio
+st.set_page_config(layout= 'wide')
+col1, empty_col, col2 = st.columns([1.5, 0.5, 1.5])  # Adjust column width ratio.
 
 with col1:
-    st.image("images/photo.jpg", width=250)  # Set a reasonable width for web display
+    st.image("images/photo.jpg", width=700)  # Set a reasonable width for web display
 
 with col2:
     st.title("Kevin KAMDEM")
@@ -21,3 +22,30 @@ with col2:
     st.write(content)
 
 st.write("🚀 You can find the apps I have built below. Feel free to contact me!")
+
+col3, col4 = st.columns(2)
+df = pandas.read_csv('data.csv', sep=';')
+
+with col3:
+#    for value in df[df.columns[0]]:
+#        st.header(value)
+    for index, row in df[:10].iterrows():
+        st.header(row['title'])
+        st.write(row['description'])
+        st.image(f"images/{row['image']}")
+        st.write (f"[Source Code]({row['url']})",unsafe_allow_html=True)
+
+
+with col4:
+
+    for index, row in df[10:].iterrows():
+        st.header(row['title'])
+        st.write(row['description'])
+        st.image(f"images/{row['image']}")
+        st.write (f"[Source Code]({row['url']})",unsafe_allow_html=True)
+
+
+#st.write(df)
+#with col4:
+ #   for index, row in df.iterrows():
+ #       print(row)

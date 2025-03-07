78c9121c6e43c67b647d0963bb726d2e7000b6c6
Kevin <k3v1n.k4md3m@gmail.com>
2025-03-07 15:57:35 +0100

    sen an email

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
 st.header('Contact Me')
+
+with st.form(key='email_forms'):
+    user_email = st.text_input('your email address')
+    message = st.text_area('your message')
+    button = st.form_submit_button()
+    if button:
+        message = message +user_email
+        print('button was pressed!')
---
+++ send_email.py
@@ -0,0 +1,21 @@
+import smtplib
+import ssl
+
+host = 'smtp.gmail.com'
+port = 465
+
+
+password = 'sbzv bboj uhvh ynlh' # not recommended
+user_name = 'emlyssaker@gmail.com'
+context = ssl.create_default_context()
+message = """\
+Subject: send an email using Python
+
+Hi,
+Hello Wordl
+Bye!
+"""
+receiver = 'emlyssaker@gmail.com'
+with smtplib.SMTP_SSL(host, port, context=context) as server:
+    server.login(user_name, password)
+    server.sendmail(user_name, receiver, message)
